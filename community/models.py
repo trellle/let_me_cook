786da3cba1a90099208cf77f9cd3d21532d77ea2
yarik <super.racing@ukr.net>
2025-11-16 20:37:17 +0200

    created models for community app

--- community/models.py
+++ community/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Currency(models.Model):
+    name = models.CharField(max_length=10)
+    rate_to_usdt = models.DecimalField(max_digits=10, decimal_places = 7)
+
+
+class Achievement(models.Model):
+    label = models.CharField(max_length=128)
+    description = models.TextField(null=False, blank=True)
+    pictogram = models.ImageField()
+    fetch_time = models.DateTimeField()
+
+
+class Cooker(AbstractUser):
+    avatar = models.ImageField()
+    rank = models.CharField(max_length=128)
+    achievements = models.ManyToManyField(Achievement, related_name="cookers")
+    currency = models.ForeignKey(Currency,
+                                 on_delete=models.SET_NULL,
+                                 null=True, blank=True,
+                                 related_name="person")
+
+    
+class Post(models.Model):
+    author = models.ForeignKey(Cooker, on_delete=models.CASCADE, related_name="posts")
+    created_time = models.DateTimeField(auto_now_add=True)
+    dish = models.ForeignKey("receipts.Dish", on_delete=models.CASCADE, related_name="comments")
+    rate = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ]
+    )
+    pros = models.TextField(blank=True)
+    cons = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    media = models.ImageField(null=True, blank=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Cooker, on_delete=models.CASCADE, related_name="comments")
+    create_time = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(null=False, blank=False)
