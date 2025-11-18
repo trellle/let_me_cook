c9a73bf440b34e2a39cc9336d38d3addbe6be8f6
yarik <super.racing@ukr.net>
2025-11-18 15:07:58 +0200

    created CRUD for posts and wrote logic for comments

--- community/views.py
+++ community/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views import generic
+from community.models import Post, Comment
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-created_time")
+    template_name = "receipts/dish_detail.html"
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    template_name = "receipts/dish_detail.html"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    queryset = Post.objects.all().prefetch_related("comments")
+
+
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+
+
+class SendCommentView(LoginRequiredMixin, generic.View):
+    model = Comment
+    fields = ["message"]
+
+    def post(self, request, pk):
+        post = Post.objects.get(id=pk)
+        comment = (Post.objects.
+                   create(post=post, content=request.POST.get("content")))
+        comment.save()
+        return redirect("receipts:post-detail")
+
+
+class DeleteCommentView(LoginRequiredMixin, generic.DeleteView):
+    model = Comment
