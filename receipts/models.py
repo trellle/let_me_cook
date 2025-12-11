81ee1c0115b61d189bdf89aff7599bbc815e236d
yarik <super.racing@ukr.net>
2025-12-11 13:23:54 +0200

    added validators&friends field

--- receipts/models.py
+++ receipts/models.py
@@ -1,21 +1,15 @@
 from django.db import models
 from community.models import Cooker
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+from datetime import time
 
 
 class Equipment(models.Model):
     name = models.CharField(max_length=25)
     model = models.CharField(max_length=128, null=True, blank=True)
     category = models.CharField(max_length=128) # choice field
-    amount = models.IntegerField()
-
-    class Meta:
-            constraints = [
-                models.CheckConstraint(
-                    check=models.Q(amount__gte=1),
-                    name="amount_value_check"
-                )
-            ]
+    amount = models.IntegerField(validators=[MinValueValidator(1)])
 
 
 class IngredientCategory(models.Model):
@@ -34,10 +28,22 @@ class Receipt(models.Model):
 
 
 class Step(models.Model):
+    def check_duration(value):
+        if value <= time(0, 0, 0):
+            raise ValidationError("Duration must be greater than 0.")
+
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE, related_name="steps")
-    duration = models.TimeField()
+    duration = models.TimeField(validators=[check_duration])
     content = models.TextField()
     template = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(duration__gt="00:00:00"),
+                name="duration_positive"
+            )
+        ]
 
 
 class Measure(models.Model):
@@ -48,10 +54,22 @@ class Measure(models.Model):
 
 
 class IngredientsAmount(models.Model):
+    def check_amount(value):
+        if value <= 0.0:
+            raise ValidationError("Amount must be greater than 0.")
+
     ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[check_amount])
     measure = models.ForeignKey(Measure, on_delete=models.PROTECT)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(amount__gt=0.0),
+                name="amount_positive"
+            )
+        ]
 
 
 class Dish(models.Model):
@@ -90,17 +108,17 @@ class Category(models.Model):
 
 
 class Institution(models.Model):
-     photo = models.ImageField(null=True)
-     name = models.CharField(max_length=128)
-     shedule = models.DateTimeField()
-     country = models.CharField(max_length=128)
-     city = models.CharField(max_length=128)
-     address = models.CharField(max_length=256)
+    photo = models.ImageField(null=True)
+    name = models.CharField(max_length=128)
+    shedule = models.DateTimeField()
+    country = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    address = models.CharField(max_length=256)
 
 
 class Shop(Institution):
-     assorti = models.ManyToManyField(Ingredient, related_name="shops")
+    assorti = models.ManyToManyField(Ingredient, related_name="shops")
 
 
 class Restaurant(Institution):
-     menu = models.ManyToManyField(Dish, related_name="restaurants")
+    menu = models.ManyToManyField(Dish, related_name="restaurants")
